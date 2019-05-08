4076f534e580285e93ebce5e5ed581f8899f5376
devtestIS <ig.suhanoff2015@yandex.ru>
2019-05-08 15:18:04 +0400

    second variant

--- slider/src/js/slider/slider.js
+++ slider/src/js/slider/slider.js
@@ -1,164 +1,93 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-    const className = 'promo-slider';
-    const sliderClass = 'slider';
-    const sliders = [...document.querySelectorAll(`.${className}`)];
-    let animation = true;
-    const animationCarousel = (slider, index) => {
-        const items = [...slider.children];
-        const active = items.find(element => element.classList.contains('active'));
-        const nextActive = items[index];
-        const currentIndex = items.indexOf(active);
-        const direction = index > currentIndex;
-        const carouselElements = direction ?  items.slice(currentIndex, index + 1) : items.slice(index, currentIndex + 1);
-        const animateObserve = () => {
+    const init = () => {
+        const className = 'promo-slider';
+        const sliderClass = 'slider';
+        const sliders = [...document.querySelectorAll(`.${className}`)];
+        const sliderAnimation = (slider, ratio) => {
+            const currentTransform = Number(slider.style.transform.split(/ /)[0].replace(/[^-.0-9]/g,''));
+            const items = [...slider.children];
+            const width = slider.getBoundingClientRect().width/items.length;
+            const translateX = currentTransform + ratio * width;
             const options = {
-                root: slider,
+                root: slider.parentElement,
                 rootMargin: '0px',
                 threshold: 1.0
             };
-            
             const io = new IntersectionObserver((entries, observer) => {
                 entries.forEach(entry => {
+                    const target = entry.target;
                     if (entry.isIntersecting) {
-                        carouselElements.forEach((element, index) => {
-                            element.style.transform = `translateX(${(direction ? -1 : 1) * (direction ? carouselElements.length - 1 : index) * 100}%)`;
-                            element.addEventListener('transitionend',  function _listener() {
-                                element.classList.remove('animate');
-                                element.removeAttribute('style');
-                                element.removeEventListener('transitionend', _listener);
-                            });
+                        entries.forEach(element => {
+                            element.target.classList.remove('active')
                         });
-                        observer.unobserve(entry.target);
+                        target.classList.add('active');
+                        observer.unobserve(target);
                     }
                 });
                 
             }, options);
-            io.observe(active);
-            io.observe(nextActive);
-        };
-        active.classList.remove('active');
-        carouselElements.forEach((element, index) => {
-            element.classList.add('animate');
-            if (!direction) {
-                element.style.marginLeft = `-${(index) * 100}%`;
-            }
-        });
-        animateObserve();
-        nextActive.classList.add('active');
-    };
-    const animationSlider = (slider, direction) => {
-        animation = false;
-        const forward = direction === 'right';
-        const items = [...slider.children];
-        const active = items.find(element => element.classList.contains('active'));
-        const nextSibling = active.nextElementSibling || slider.appendChild(items[0]);
-        const prevSibling = active.previousElementSibling || slider.insertBefore(slider.lastChild, active);
-        const activeNext = forward ? nextSibling : prevSibling;
-        const directionClass = forward ? 'slider-forward' : 'slider-backwards';
-        if (!forward) {
-            activeNext.classList.add('backward');
-        }
-        active.classList.remove('active');
-        active.classList.add('animate');
-        activeNext.classList.add('active');
-        activeNext.classList.add('animate');
-
-        const animateObserve = () => {
-            const options = {
-                root: slider,
-                rootMargin: '0px',
-                threshold: 1.0
-            };
-            
-            const io = new IntersectionObserver((entries, observer) => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        active.classList.add(directionClass);
-                        activeNext.classList.add(directionClass);
-                        activeNext.addEventListener('transitionend', function _listener() {
-                            activeNext.classList.remove(directionClass, 'animate', 'backward');
-                            active.classList.remove(directionClass, 'animate');
-                            animation = true;
-                            activeNext.removeEventListener('transitionend', _listener);   
-                        });
-                        observer.unobserve(entry.target);
-                    }
+            const range = Number(slider.style.transform.split(/ /)[0].replace(/[^\d]/g, '')) >= width;
+            slider.addEventListener('transitionend', function _listener() {
+                items.forEach(element => {
+                    io.observe(element);
                 });
-                
-            }, options);
-            io.observe(active);
-        };
-
-        if(!('IntersectionObserver' in window)) {
-            import('intersection-observer').then(() => {
-                animateObserve();
+                slider.removeEventListener('transitionend', _listener);
             });
-        } else {
-            animateObserve();
-        }
-    };
-    const buttonAction = (container, slider) => {
-        container.addEventListener('click', (e) => {
-            e.preventDefault();
-            const target = e.target;
-            const direction = target.dataset.direction;
-            if (target.classList.contains(`${sliderClass}__button`) && animation) {
-                animationSlider(slider, direction);
-            }   
+            slider.style.transform = `translateX(${translateX}px)`;
+        };
+        const buttonAction = (container, slider) => {
+            container.addEventListener('click', (e) => {
+                e.preventDefault();
+                const target = e.target;
+                const direction = target.dataset.direction;
+                if (target.classList.contains(`${sliderClass}__button`)) {
+                    sliderAnimation(slider, direction === 'right' ? -1 : 1);
+                }   
+            });
+        };
+
+        const createButton = (slider) => {
+            const buttonLeft = document.createElement('button');
+            const buttonRight = document.createElement('button');
+            buttonLeft.innerText = 'left';
+            buttonRight.innerText = 'right';
+            buttonLeft.dataset.direction = 'left';
+            buttonRight.dataset.direction = 'right';
+            buttonLeft.classList.add(`${sliderClass}__button`, `${sliderClass}__button_left`);
+            buttonRight.classList.add(`${sliderClass}__button`, `${sliderClass}__button_right`);
+            slider.appendChild(buttonLeft);
+            slider.appendChild(buttonRight);
+        };
+        const createSlider = (slider) => {
+            const items = [...slider.children];
+            const sliderWrapper = document.createElement('div');
+            const sliderWidth = slider.getBoundingClientRect().width;
+            sliderWrapper.style.width = `${sliderWidth * items.length}px`;
+            items.forEach(element => {
+                element.style.width = `${sliderWidth}px`;
+            });
+            slider.classList.add(`${sliderClass}-container`);
+            sliderWrapper.classList.add(sliderClass);
+            slider.appendChild(sliderWrapper);
+            items.forEach((element) => {
+                element.classList.add(`${sliderClass}__item`);
+                sliderWrapper.appendChild(slider.removeChild(element));
+            });
+            items[0].classList.add('active');
+            return sliderWrapper;
+        };
+        sliders.forEach(element => {
+            const sliderWrapper = createSlider(element);
+            createButton(element); 
+            buttonAction(element, sliderWrapper);
         });
     };
-    const dotAction = (container, slider, dots) => {
-        container.addEventListener('click', (e) => {
-            e.preventDefault();
-            const target = e.target;
-            if (target.classList.contains('dots__item') && animation) {
-                const currentIndex = [...dots.children].indexOf(target);
-                animationCarousel(slider, currentIndex);
-            }   
-        })
-    };
-    const createDots = (slider, sliderWrapper) => {
-        const items = [...sliderWrapper.children];
-        const dotWrapper = document.createElement('ul');
-        dotWrapper.classList.add(`dots`);
-        for (let index = 0; index < items.length; index++) {
-            const dot = document.createElement('li');
-            dot.classList.add(`dots__item`);
-            dotWrapper.appendChild(dot);            
-        }
-        slider.appendChild(dotWrapper);
-        return dotWrapper;
-    };
-    const createButton = (slider) => {
-        const buttonLeft = document.createElement('button');
-        const buttonRight = document.createElement('button');
-        buttonLeft.innerText = 'left';
-        buttonRight.innerText = 'right';
-        buttonLeft.dataset.direction = 'left';
-        buttonRight.dataset.direction = 'right';
-        buttonLeft.classList.add(`${sliderClass}__button`, `${sliderClass}__button_left`);
-        buttonRight.classList.add(`${sliderClass}__button`, `${sliderClass}__button_right`);
-        slider.appendChild(buttonLeft);
-        slider.appendChild(buttonRight);
-    };
-    const createSlider = (slider) => {
-        const items = [...slider.children];
-        const sliderWrapper = document.createElement('div');
-        slider.classList.add(`${sliderClass}-container`);
-        sliderWrapper.classList.add(sliderClass);
-        slider.appendChild(sliderWrapper);
-        items.forEach((element) => {
-            element.classList.add(`${sliderClass}__item`);
-            sliderWrapper.appendChild(slider.removeChild(element));
+    if(!('IntersectionObserver' in window)) {
+        import('intersection-observer').then(() => {
+            init();
         });
-        items[0].classList.add('active');
-        return sliderWrapper;
-    };
-    sliders.forEach(element => {
-        const sliderWrapper = createSlider(element);
-        const dots = createDots(element, sliderWrapper);
-        createButton(element); 
-        buttonAction(element, sliderWrapper);
-        dotAction(element, sliderWrapper, dots);
-    });
+    } else {
+        init();
+    }
+    
 });
