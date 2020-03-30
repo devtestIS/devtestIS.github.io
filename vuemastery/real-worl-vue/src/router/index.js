373f287e293bb660ba678710eee78847bfd5dc2f
Игорь Суханов <ig.suhanoff2015@yandex.ru>
2020-03-30 23:34:10 +0400

    dynamic-routing-history-mode

---
+++ vuemastery/real-worl-vue/src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EventShow from "../views/EventShow.vue";
+import EventList from "../views/EventList.vue";
+import EventCreate from "../views/EventCreate.vue";
+import User from "@/views/User.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "event-list",
+    component: EventList
+  },
+  {
+    path: "/event",
+    name: "event-show",
+    component: EventShow
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
+  },
+  {
+    path: "/user/:username",
+    name: "user",
+    component: User,
+    props: true
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ vuemastery/real-worl-vue/src/views/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user">This is {{ username }}'s page</div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style></style>
